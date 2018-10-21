64d6f181a8beedeed1e2e0fd43bbcae9322bd1b5
ranranqi <1137625794@qq.com>
2018-10-21 14:48:47 +0800

    promise直接用参数，不用起名字

--- main.js
+++ main.js
@@ -1,14 +1,8 @@
-
-function success(responseText){
-    console.log(responseText)
-}
-function fail(request){
-    console.log(request)
-}
-
 myButton.addEventListener('click',(e)=>{
     $.ajax({
-        url: '/xxx',
-        type: 'post',   
-    }).then(success,fail)
+        url: '/frank',
+        type: 'get',   
+    }).then(
+        (responseText)=>{console.log(responseText)},
+        (request)=>{console.log('error')})
 })
