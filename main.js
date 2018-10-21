9cae5a9241d999eb1fe4214da5c27f5d452efa5c
ranranqi <1137625794@qq.com>
2018-10-21 14:59:18 +0800

    对一个结果进行多次处理 再.then()

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
 myButton.addEventListener('click',(e)=>{
     $.ajax({
-        url: '/frank',
+        url: '/xxx',
         type: 'get',   
     }).then(
-        (responseText)=>{console.log(responseText)},
-        (request)=>{console.log('error')})
+        (responseText)=>{
+            console.log(responseText);
+            return responseText
+        },
+        (request)=>{
+            console.log('error1');return '已经处理'}
+        ).then(
+            (上一次的处理结果)=>{
+                console.log(上一次的处理结果)
+            },
+            (request)=>{
+                console.log('error2')
+            }
+        )
 })
