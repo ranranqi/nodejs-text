1f28d6428e96d45ca94dbce85e08053824b69f55
ranranqi <1137625794@qq.com>
2018-10-21 12:46:47 +0800

    封装简单的AJAX

--- main.js
+++ main.js
@@ -1,28 +1,34 @@
-myButton.addEventListener('click',()=>{
+window.jQuery = function(nodeOrSelector){
+    let nodes = {}
+    nodes.addClass = function(){}
+    nodes.html = function(){}
+    return nodes
+}
+
+
+window.jQuery.ajax = function(url,method,body,successFn,failFn){
     let request = new XMLHttpRequest()
-    request.open('GET','http://jack.com:8002/xxx')
-    request.send()
+    request.open(method,url)
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
-            console.log('请求的响应都完成了')
-
             if(request.status >= 200 && request.status < 300){
-                console.log('说明请求成功了')
-                console.log(typeof request.responseText)
-                console.log(request.responseText)
-                let string = request.responseText
-                //把符合JSON语法的字符床转换成JS对应的值（对象或数组）
-                let object = window.JSON.parse(string)
-                console.log(typeof object)
-                console.log(object)
-                console.log('object.note')
-                console.log(object.note)
-                console.log('object.note.body')
-                console.log(object.note.body)
-
+                successFn.call(undefined,request.responseText)
             }else if(request.status >= 400){
-                console.log('说明请求失败')
+                failFn.call(undefined,request)
             }
         }
     }
+    request.send(body) 
+}
+
+window.$ = window.jQuery
+
+myButton.addEventListener('click',(e)=>{
+    window.jQuery.ajax(
+        '/xxx',
+        'post',
+        'a=1&b=2',
+        (responseText)=>{ console.log(responseText) },
+        (request)=>{ console.log(2) }
+    )
 })
