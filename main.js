885346e3982707580544f3eb0a095918999d266c
ranranqi <1137625794@qq.com>
2018-10-21 13:42:13 +0800

    一个函数接受两种参数

--- main.js
+++ main.js
@@ -7,12 +7,19 @@ window.jQuery = function(nodeOrSelector){
 
 
 window.jQuery.ajax = function(options){
-    let url = options.url
+    let url
+    if(arguments.length === 1){
+        url = options.url
+    }else if(arguments.length === 2){
+        url = arguments[0]
+        options = arguments[1]
+    }
     let method = options.method
     let body = options.body
     let successFn = options.successFn
     let failFn = options.failFn
     let headers = options.headers
+    
 
     let request = new XMLHttpRequest()
     request.open(method,url)
