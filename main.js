65b14d9bf44388430b134887d5b3390c92690a73
ranranqi <1137625794@qq.com>
2018-10-21 13:32:31 +0800

    设置header

--- main.js
+++ main.js
@@ -12,9 +12,14 @@ window.jQuery.ajax = function(options){
     let body = options.body
     let successFn = options.successFn
     let failFn = options.failFn
+    let headers = options.headers
 
     let request = new XMLHttpRequest()
     request.open(method,url)
+    for(let key in headers){ // 遍历headers
+        let value = headers[key]
+        request.setRequestHeader(key,value)
+    }
     request.onreadystatechange = ()=>{
         if(request.readyState === 4){
             if(request.status >= 200 && request.status < 300){
@@ -37,6 +42,10 @@ myButton.addEventListener('click',(e)=>{
     window.jQuery.ajax({
         url: '/frank',
         method: 'get',
+        headers: {
+            'content-type':'application/x-www-form-urlencoded',
+            'frank':'18'
+        },
         successFn: (x)=>{  //这儿函数里的参数 x 就是request.responseText。 是一个callback回调函数
             f1.call(undefined,x)
             f2.call(undefined,x)  
