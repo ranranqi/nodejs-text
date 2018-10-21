0306e588655cee60a1972e19e874c606e823599f
ranranqi <1137625794@qq.com>
2018-10-21 14:41:18 +0800

    Promise写法

--- main.js
+++ main.js
@@ -1,19 +1,14 @@
 
-function f1(responseText){}
-function f2(responseText){}
+function success(responseText){
+    console.log(responseText)
+}
+function fail(request){
+    console.log(request)
+}
 
 myButton.addEventListener('click',(e)=>{
-    window.jQuery.ajax({
-        url: '/frank',
-        type: 'get',
-        success: (x)=>{  //这儿函数里的参数 x 就是request.responseText。 是一个callback回调函数
-            f1.call(undefined,x)
-            f2.call(undefined,x)  
-        }, 
-        error: (x)=>{  //这儿的 x 就是request。 是一个callback回调函数
-            console.log(x)
-            console.log(x.status)
-            console.log(x.responseText)   
-        }   
-    })
+    $.ajax({
+        url: '/xxx',
+        type: 'post',   
+    }).then(success,fail)
 })
