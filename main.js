476eb608aeb080ad1c4a64e855924afc6dc60d43
ranranqi <1137625794@qq.com>
2018-10-21 13:24:41 +0800

    成功之后执行两个函数

--- main.js
+++ main.js
@@ -29,12 +29,17 @@ window.jQuery.ajax = function(options){
 
 window.$ = window.jQuery
 
+
+function f1(responseText){}
+function f2(responseText){}
+
 myButton.addEventListener('click',(e)=>{
     window.jQuery.ajax({
         url: '/frank',
         method: 'get',
         successFn: (x)=>{  //这儿函数里的参数 x 就是request.responseText。 是一个callback回调函数
-            console.log(x)  
+            f1.call(undefined,x)
+            f2.call(undefined,x)  
         }, 
         failFn: (x)=>{  //这儿的 x 就是request。 是一个callback回调函数
             console.log(x)
